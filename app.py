1a91cc2d6de8ab0b7e03020a1832e114667f9ef7
mbobna <bobnaric.mitja@gmail.com>
2025-06-19 17:34:43 +0200

    add hobies

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
+import datetime
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    text = "We are living in "
+    current_year = datetime.datetime.now().year
+    return render_template("index.html", text=text, current_year=current_year)
 
+@app.route("/about-me")
+def home():
+    hobbies = ["cycling", "cooking", "Hiking"]
+    return render_template("about.html", hobbies=hobbies)
 
 if __name__ == '__main__':
     app.run(use_reloader=True)
