b25299b6f57b0c674a518930ad6f2ed313a6721d
mbobna <bobnaric.mitja@gmail.com>
2025-06-21 14:40:27 +0200

    add cookies

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import datetime
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, make_response
 
 app = Flask(__name__)
 
@@ -10,10 +10,22 @@ def index():
     current_year = datetime.datetime.now().year
     return render_template("index.html", text=text, current_year=current_year)
 
-@app.route("/about-me")
+
+@app.route("/about-me", methods=["GET", "POST"])
 def home():
-    hobbies = ["cycling", "cooking", "Hiking"]
-    return render_template("about.html", hobbies=hobbies)
+    if request.method == "GET":
+        user_name = request.cookies.get("user_name")
+        hobbies = ["cycling", "cooking", "Hiking"]
+        return render_template("about.html", hobbies=hobbies, user_name=user_name)
+    elif request.method == "POST":
+        name = request.form.get("contact-name")
+        email = request.form.get("contact-email")
+        message = request.form.get("contact-message")
+
+        response = make_response(render_template("contact.html"))
+        response.set_cookie("user_name", name)
+
+        return response
 
 if __name__ == '__main__':
     app.run(use_reloader=True)
